5b930a3b95dbd1fe790ad209d1700e4770278be8
unknown <daithang26011997@gmai.com>
2020-08-11 11:32:19 +0300

    Updates

---
+++ precache-manifest.12e2606a75acd476223153e383d8d30c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7c631819639ef4c09cba89e251f9b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d20c7e56cc99a115fbc2",
+    "url": "/static/css/main.28456cb7.chunk.css"
+  },
+  {
+    "revision": "4d965974964e6b7339e7",
+    "url": "/static/js/2.248fd9d7.chunk.js"
+  },
+  {
+    "revision": "6993fa7b13fb81f4bc70d0dc713d6897",
+    "url": "/static/js/2.248fd9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d20c7e56cc99a115fbc2",
+    "url": "/static/js/main.1f7d351b.chunk.js"
+  },
+  {
+    "revision": "1dc3b027e697dd836b3e",
+    "url": "/static/js/runtime-main.5b7c67a2.js"
+  }
+]);
